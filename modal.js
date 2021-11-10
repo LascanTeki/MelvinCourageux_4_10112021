c5bf638f633c83b01c62994712b1630288c0f17e
LascanTeki <dieageux@yahoo.fr>
2021-11-10 17:52:52 +0100

    files

---
+++ modal.js
@@ -0,0 +1,31 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const closeMod = document.querySelectorAll(".close");
+const formData = document.querySelectorAll(".formData");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// launch modal close
+closeMod.forEach((close) => close.addEventListener("click", closeModal));
+
+// close modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
